63c730b6ebe966309a1ab88eac2d66136f95c06b
Florian Eiden <florian.eiden@fleid.fr>
2015-07-01 11:31:43 +0200

    12.2

--- Ex12/BooleanExpressions.java
+++ Ex12/BooleanExpressions.java
@@ -11,6 +11,23 @@ public class BooleanExpressions
 		double x, y;
 		
 		System.out.pint( "Give me two numbers. First: " );
+		x = keyboard.nextDouble();
+		System.out.print( "Second: " );
+		y = keyboard.nextDouble();
 		
+		a = (x <  y);
+		b = (x <= y);
+		c = (x == y);
+		d = (x != y);
+		e = (x >  y);
+		f = (x >= y);
+		
+		System.out.println( x + " is LESS THAN " + y + ": " + a );
+		System.out.println( x + " is LESS THAN or EQUAL TO " + y + ": " + b );
+		System.out.println( x + " is EQUAL TO " + y + ": " + c );
+		System.out.println( x + " is NOT EQUAL TO " + y + ": " + d );
+		System.out.println( x + " is GREATER THAN " + y + ": " + e );
+		System.out.println( x + " is GREATHER THAN or EQUAL TO " + y + ": " + f );
+		System.out.println();
 	}
 }
