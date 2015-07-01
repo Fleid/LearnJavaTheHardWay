f5a9d35493164bbdc583185a78d14fdced1c881a
Florian Eiden <florian.eiden@fleid.fr>
2015-07-01 11:24:46 +0200

    12.1

---
+++ Ex12/BooleanExpressions.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class BooleanExpressions
+{
+	public static void main( String[] args )
+	{
+		Scanner keyboard = new Scanner(System.in).useLocale(Locale.ENGLISH);
+		
+		boolean a, b, c, d, e, f;
+		double x, y;
+		
+		System.out.pint( "Give me two numbers. First: " );
+		
+	}
+}
