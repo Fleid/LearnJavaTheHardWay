287e8cf1eef1e1fd52c91fc29a3417fd8eec7390
Florian Eiden <florian.eiden@fleid.fr>
2015-07-08 17:26:57 +0200

    PP

--- EX15/AgeMessages.java
+++ EX15/AgeMessages.java
@@ -6,12 +6,12 @@ public class AgeMessages
 	public static void main( String[] args )
 	{
 		Scanner keyboard = new Scanner(System.in).useLocale(Locale.ENGLISH);
-		
+
 		int age;
-		
+
 		System.out.print( "How old are you? ");
 		age = keyboard.nextInt();
-		
+
 		if ( age < 18 )
 			{
 				System.out.println( "You are too young to get a tattoo. ");
